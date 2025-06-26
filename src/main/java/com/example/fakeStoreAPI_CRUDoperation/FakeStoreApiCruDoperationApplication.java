f7a2bebc4ee1848288e38cefcf1aa0991a2903a7
abhinav84094 <abhinav84094@gmail.com>
2025-06-27 03:06:30 +0530

    Basic springboot setup

---
+++ src/main/java/com/example/fakeStoreAPI_CRUDoperation/FakeStoreApiCruDoperationApplication.java
@@ -0,0 +1,13 @@
+package com.example.fakeStoreAPI_CRUDoperation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreApiCruDoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FakeStoreApiCruDoperationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fakeStoreAPI_CRUDoperation/FakeStoreApiCruDoperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fakeStoreAPI_CRUDoperation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreApiCruDoperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
