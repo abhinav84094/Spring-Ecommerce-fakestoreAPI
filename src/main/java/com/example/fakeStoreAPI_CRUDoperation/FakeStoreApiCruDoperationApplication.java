8716c9ef207a66dee68e8b0c732f01fde864fbd8
abhinav84094 <abhinav84094@gmail.com>
2025-07-02 01:58:36 +0530

    dotenv setup added

--- src/main/java/com/example/fakeStoreAPI_CRUDoperation/FakeStoreApiCruDoperationApplication.java
+++ src/main/java/com/example/fakeStoreAPI_CRUDoperation/FakeStoreApiCruDoperationApplication.java
@@ -3,10 +3,18 @@ package com.example.fakeStoreAPI_CRUDoperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
+
+
 @SpringBootApplication
 public class FakeStoreApiCruDoperationApplication {
 
 	public static void main(String[] args) {
+		
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach( entry -> System.setProperty(entry.getKey(), entry.getValue()));
+		
 		SpringApplication.run(FakeStoreApiCruDoperationApplication.class, args);
 	}
 
